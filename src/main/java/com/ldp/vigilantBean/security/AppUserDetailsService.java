3745e911a626c4ce3aedb77e3efe5edc9107887e
Lyakhnitskiy Dmitry <LyakhnitskiyDP@gmail.com>
2021-01-25 12:58:14 +0300

    [security] Add UserDetailsService impl

---
+++ src/main/java/com/ldp/vigilantBean/security/AppUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.ldp.vigilantBean.security;
+
+import com.ldp.vigilantBean.domain.appUser.AppUser;
+import com.ldp.vigilantBean.repository.AppUserRetrievalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+public class AppUserDetailsService implements UserDetailsService {
+
+    private AppUserRetrievalRepository appUserRetrievalRepository;
+
+    public AppUserDetailsService(
+            @Autowired
+            AppUserRetrievalRepository appUserRetrievalRepository) {
+
+        this.appUserRetrievalRepository = appUserRetrievalRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        Optional<AppUser> optUser =
+                appUserRetrievalRepository.getUserByUsername(username);
+
+        UserDetails userDetails =
+                new AppUserDetails(optUser.orElseThrow(
+                        () -> new UsernameNotFoundException(username + " cannot be found") )
+                );
+
+        return userDetails;
+    }
+}
