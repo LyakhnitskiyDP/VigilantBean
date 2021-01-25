5a41d8c1234599d64c82ce44d86ec81358780417
Lyakhnitskiy Dmitry <LyakhnitskiyDP@gmail.com>
2021-01-25 16:02:07 +0300

    [security] Configure auth manager to use auth provider

--- src/main/java/com/ldp/vigilantBean/config/WebSecurityConfig.java
+++ src/main/java/com/ldp/vigilantBean/config/WebSecurityConfig.java
@@ -1,20 +1,48 @@
 package com.ldp.vigilantBean.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationProvider authenticationProvider;
+
+    public WebSecurityConfig(
+            @Autowired
+            AuthenticationProvider authenticationProvider) {
+
+    }
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeRequests()
-                    .antMatchers("/api/v*/registration/**")
+                    .antMatchers("/signUp/**")
                     .permitAll();
 
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Bean(name = "sCryptPasswordEncoder")
+    public PasswordEncoder sCryptPasswordEncoder() {
+
+        return new SCryptPasswordEncoder();
     }
 
 
