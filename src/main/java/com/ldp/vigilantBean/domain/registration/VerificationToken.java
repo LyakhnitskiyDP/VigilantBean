121612cbd48ce9ff6c6741a848888ba1ba23fee1
Lyakhnitskiy Dmitry <LyakhnitskiyDP@gmail.com>
2021-01-27 18:40:51 +0300

    [registration] Add VerificationToken

--- src/main/java/com/ldp/vigilantBean/domain/registration/VerificationToken.java
+++ src/main/java/com/ldp/vigilantBean/domain/registration/VerificationToken.java
@@ -37,16 +37,49 @@ public class VerificationToken implements Serializable {
 
     public VerificationToken(Date initialDate) {
 
-        LocalDateTime initialDateTime = ChronoUtils.
+        LocalDateTime initialDateTime =
+                ChronoUtils.convertToLocalDateTime(initialDate);
 
         LocalDateTime expiryDateTime =
                 initialDateTime.plus(EXPIRATION_IN_MINUTES, ChronoUnit.MINUTES);
 
         this.expiryDate =
-                Date.from(expiryDateTime.atZone(ZoneId.systemDefault()).toInstant());
+                ChronoUtils.convertToDate(expiryDateTime);
     }
 
+    public VerificationToken() {
+        this(new Date());
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
 }
